4bebc592f282228e604688dd2b0244fdc612e47a
Heli Türi <heli.turi@voco.ee>
2023-04-05 15:07:33 +0300

    Ex 4 and Ex 5 - ok + test of picture values

--- resistorrun.ts
+++ resistorrun.ts
@@ -1,104 +1,86 @@
 class Resistor {
     r: number = 0;
-    allowedPower: number = 0
-    constructor(r: number, ap: number) {
-        constructor(r: number) {
-            this.r = r;
-            this.allowedPower = ap
-        }
-        getCurrent(u: number): number {
-            return u / this.r;
-        }
-        getPower(u: number): number {
-            return u * this.getCurrent(u);
-        }
-        getResistance(): number {
-            return this.r;
-        }
+    constructor(r: number) {
+        this.r = r;
     }
-
-    let r1 = new Resistor(220, 0.2);
-    let r2 = new Resistor(110, 0.1);
-    let r3 = new Resistor(4700, 5);
-
-    let resistors: Resistor[] = []
-    resistors = [r1, r2, r3]
-
-    function controlResistorAllowed(resistors: Resistor[], u: number): Resistor[] {
-    let allowedResistors: Resistor[] = []
-    resistors.forEach((resistor) => {
-        if(resistor.getPower(u) <= resistor.allowedPower){
-            allowedResistors.push(resistor);
-        }
-    })
-    return allowedResistors
-    class SeriesCircuit {
-        resistors: Resistor[] = []
-        push(r: Resistor) {
-            this.resistors.push(r);
-        }
-        getTotalResistance() {
-            let sum: number = 0;
-            this.resistors.forEach((r: Resistor) => { sum += r.getResistance() });
-            return sum;
-        }
-        getCurrent(u: number) {
-            return u / this.getTotalResistance();
-        }
-
-        getTotalPower(u: number){
-            return u * this.getCurrent(u)
-        }
-
-        getBiggestResistance(): Resistor{
-            this.resistors.sort((a, b) => (a.getResistance() > b.getResistance()) ? 1 : -1).reverse()
-            return this.resistors[0]
-        }
-
-        getBiggestPotential(u: number): number{
-            let biggestPotential: number = 0
-            this.resistors.forEach((resistor) => {
-                let resistorPotential: number = this.getCurrent(u) * resistor.getResistance()
-                if(resistorPotential > biggestPotential){
-                    biggestPotential = resistorPotential
-                }
-            })
-            return  biggestPotential
-        }
-
-        getBiggestPower(u: number): number{
-            let biggestPower: number = 0
-            this.resistors.forEach((resistor) => {
-                let resistorPotential: number = this.getCurrent(u) * resistor.getResistance()
-                let resistorPower: number = resistorPotential * this.getCurrent(u)
-                if(resistorPower > biggestPower){
-                    biggestPower = resistorPower
-                }
-            })
-            return  biggestPower
-        }
+    getCurrent(u: number): number {
+        return u / this.r;
     }
+    getPower(u: number): number {
+        return u * this.getCurrent(u);
+    }
+    getResistance(): number {
+        return this.r;
+    }
+}
+class SeriesCircuit {
+    resistors: Resistor[] = []
+    push(r: Resistor) {
+        this.resistors.push(r);
+    }
+    getTotalResistance() {
+        let sum: number = 0;
+        this.resistors.forEach((r: Resistor) => { sum += r.getResistance() });
+        return sum;
+    }
+    getCurrent(u: number) {
+        return u / this.getTotalResistance();
+    }
+    getTotalPower(u: number){
+        return u * this.getCurrent(u)
+    }
+    getBiggestResistance(): Resistor{
+        this.resistors.sort((a, b) => (a.getResistance() > b.getResistance()) ? 1 : -1).reverse()
+        return this.resistors[0]
+    }
+    getBiggestPotential(u: number): number{
+        let biggestPotential: number = 0
+        this.resistors.forEach((resistor) => {
+            let resistorPotential: number = this.getCurrent(u) * resistor.getResistance()
+            if(resistorPotential > biggestPotential){
+                biggestPotential = resistorPotential
+            }
+        })
+        return  biggestPotential
+    }
+    getBiggestPower(u: number): number{
+        let biggestPower: number = 0
+        this.resistors.forEach((resistor) => {
+            let resistorPotential: number = this.getCurrent(u) * resistor.getResistance()
+            let resistorPower: number = resistorPotential * this.getCurrent(u)
+            if(resistorPower > biggestPower){
+                biggestPower = resistorPower
+            }
+        })
+        return  biggestPower
+    }
+}
+let sc1: SeriesCircuit = new SeriesCircuit();
+sc1.push(new Resistor(220));
+sc1.push(new Resistor(110));
+sc1.push(new Resistor(220));
+console.log(sc1.getTotalResistance());
+console.log(sc1.getCurrent(12));
+console.log(sc1.getTotalPower(12))
+console.log(sc1.getBiggestResistance())
+console.log(sc1.getBiggestPotential(12))
+console.log(sc1.getBiggestPower(12))
+let sc2: SeriesCircuit = new SeriesCircuit();
+sc2.push(new Resistor(220));
+sc2.push(new Resistor(220));
+sc2.push(new Resistor(220));
+console.log(sc2.getTotalResistance());
+console.log(sc2.getCurrent(12));
+console.log(sc2.getTotalPower(12))
+console.log(sc2.getBiggestResistance())
+console.log(sc2.getBiggestPotential(12))
+console.log(sc2.getBiggestPower(12))
 
-    console.log(controlResistorAllowed(resistors, 5))
-    let sc1: SeriesCircuit = new SeriesCircuit();
-    sc1.push(new Resistor(220));
-    sc1.push(new Resistor(110));
-    sc1.push(new Resistor(220));
-
-    console.log(sc1.getTotalResistance());
-    console.log(sc1.getCurrent(12));
-    console.log(sc1.getTotalPower(12))
-    console.log(sc1.getBiggestResistance())
-    console.log(sc1.getBiggestPotential(12))
-    console.log(sc1.getBiggestPower(12))
-
-    let sc2: SeriesCircuit = new SeriesCircuit();
-    sc2.push(new Resistor(220));
-    sc2.push(new Resistor(220));
-    sc2.push(new Resistor(220));
-    console.log(sc2.getTotalResistance());
-    console.log(sc2.getCurrent(12));
-    console.log(sc2.getTotalPower(12))
-    console.log(sc2.getBiggestResistance())
-    console.log(sc2.getBiggestPotential(12))
-    console.log(sc2.getBiggestPower(12))
+let sc3: SeriesCircuit = new SeriesCircuit();
+sc3.push(new Resistor(100));
+sc3.push(new Resistor(200));
+console.log(sc3.getTotalResistance());
+console.log(sc3.getCurrent(5));
+console.log(sc3.getBiggestResistance())
+console.log(sc3.getBiggestPotential(5))
+console.log(sc3.getBiggestPower(5))
